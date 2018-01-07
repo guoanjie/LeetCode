b45dd3d9f96f04deccecf28ffd3ded8a0a0b736c
Eric Guo <guoanjie@gmail.com>
2018-01-06 23:13:50 -0500

    use upper_bound

--- 744.cc
+++ 744.cc
@@ -7,18 +7,8 @@ using std::vector;
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        auto beg = letters.cbegin(), end = letters.cend();
-        while (beg != end) {
-            auto mid = beg + (end - beg) / 2;
-            if (*mid > target)
-                end = mid;
-            else
-                beg = mid + 1;
-        }
-        if (beg == letters.cend())
-            return letters[0];
-        else
-            return *beg;
+        auto it = upper_bound(letters.cbegin(), letters.cend(), target);
+        return it == letters.cend() ? letters[0] : *it;
     }
 };
 
