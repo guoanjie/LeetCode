45a87946dea742a3e21dcd4646c04a054ee6a108
Eric Guo <guoanjie@gmail.com>
2018-01-20 15:54:13 -0500

    use equal_range

--- 034.cc
+++ 034.cc
@@ -1,20 +1,21 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using std::cout;
 using std::endl;
+using std::equal_range;
 using std::vector;
 
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        auto lower = lower_bound(nums.cbegin(), nums.cend(), target);
-        auto upper = upper_bound(nums.cbegin(), nums.cend(), target);
-        if (lower == upper)
+        auto range = equal_range(nums.cbegin(), nums.cend(), target);
+        if (range.first == range.second)
             return {-1, -1};
         else
             return {
-                static_cast<int>(lower - nums.cbegin()),
-                static_cast<int>(upper - nums.cbegin()) - 1
+                static_cast<int>(range.first - nums.cbegin()),
+                static_cast<int>(range.second - nums.cbegin()) - 1
             };
     }
 };
