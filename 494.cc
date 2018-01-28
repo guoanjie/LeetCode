04e989a80afef3d37a0601d62621735bf0abe0d5
Eric Guo <guoanjie@gmail.com>
2018-01-28 18:43:57 -0500

    improved efficiency for 494

--- 494.cc
+++ 494.cc
@@ -14,15 +14,19 @@ public:
         int sum = accumulate(nums.cbegin(), nums.cend(), 0);
         if ((sum - S) % 2)
             return 0;
+        cache = vector<unordered_map<int, int>>(nums.size() + 1);
         return findTargetPosSumWays(nums, (sum - S) / 2, nums.size());
     }
 private:
+    vector<unordered_map<int, int>> cache;
     int findTargetPosSumWays(const vector<int>& nums, int S, size_t sz) {
         if (S < 0)
             return 0;
         if (sz == 0)
             return S == 0 ? 1 : 0;
-        return findTargetPosSumWays(nums, S, sz - 1) + findTargetPosSumWays(nums, S - nums[sz - 1], sz - 1);
+        if (cache[sz].find(S) == cache[sz].end())
+            cache[sz][S] = findTargetPosSumWays(nums, S, sz - 1) + findTargetPosSumWays(nums, S - nums[sz - 1], sz - 1);
+        return cache[sz][S];
     }
 };
 
