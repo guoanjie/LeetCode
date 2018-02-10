a21948c6f1cae2bf4bef25687a45d939b2845948
Eric Guo <guoanjie@gmail.com>
2018-02-10 15:32:38 -0500

    use __builtin_clz

--- 342.cc
+++ 342.cc
@@ -5,7 +5,7 @@ using std::endl;
 class Solution {
 public:
     bool isPowerOfFour(int num) {
-        return num > 0 && !(num & (num - 1)) && (__builtin_popcount(num - 1) + 1) % 2;
+        return __builtin_popcount(num) == 1 && __builtin_ctz(num) % 2 == 0;
     }
 };
 
