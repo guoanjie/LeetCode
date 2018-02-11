5eb9b53fbd1b3cf34085a51c3b73c659801a1a3d
Eric Guo <guoanjie@gmail.com>
2018-02-10 23:15:26 -0500

    use bit manipulation

--- 187.cc
+++ 187.cc
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <string>
-#include <unordered_map>
 #include <vector>
 using std::cout;
 using std::endl;
 using std::string;
-using std::unordered_map;
 using std::vector;
 
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        unordered_map<string, int> count;
-        for (int i = 0; i + 10 <= s.length(); ++i)
-            ++count[s.substr(i, 10)];
-        vector<string> sequences;
-        for (auto e : count) {
-            if (e.second > 1)   sequences.push_back(e.first);
+        if (s.size() < 10)  return {};
+        int count[0xFFFFF] = {0};
+        int hash = 0;
+        for (size_t pos = 0; pos < 9; ++pos)
+            hash = hash << 2 | (s[pos] -'A' + 1) % 5;
+        vector<string> repeated;
+        for (size_t pos = 9; pos < s.size(); ++pos) {
+            hash = (hash << 2 & 0xFFFFF) | (s[pos] - 'A' + 1) % 5;
+            if (count[hash]++ == 1)
+                repeated.push_back(s.substr(pos - 9, 10));
         }
-        return sequences;
+        return repeated;
     }
 };
 
