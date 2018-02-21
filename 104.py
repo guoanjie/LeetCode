58182aa0df5864edc2cc6a995db410d7fe573b8a
Eric Guo <guoanjie@gmail.com>
2018-02-20 21:59:43 -0500

    add 104.py

---
+++ 104.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right)) if root else 0
+
+
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    print(Solution().maxDepth(root))
