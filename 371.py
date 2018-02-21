a6d3e673aed31e467e62e52e2d36e0a212199b42
Eric Guo <guoanjie@gmail.com>
2018-02-21 08:33:19 -0500

    add 371.py

---
+++ 371.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        return a + b
+
+
+if __name__ == "__main__":
+    print(Solution().getSum(1, 2))
