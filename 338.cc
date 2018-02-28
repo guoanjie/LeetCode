4fa99f73079aee55479cdfa694a2a6965e30d097
Eric Guo <guoanjie@gmail.com>
2018-02-27 20:14:32 -0500

    add 338.cc

---
+++ 338.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> bits(num + 1, 0);
+        for (int i = 1; i <= num; ++i) {
+            if (i & 1)  bits[i] = bits[i - 1] + 1;
+            else    bits[i] = bits[i / 2];
+        }
+        return bits;
+    }
+};
+
+int main() {
+    auto bits = Solution().countBits(5);
+    for (auto bit : bits)   cout << bit << ' ';
+    cout << endl;
+    return 0;
+}
