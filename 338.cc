9b45f0a1fff8f6f955fe31f0a221e9eff2cef5af
Eric Guo <guoanjie@gmail.com>
2018-02-27 20:22:35 -0500

    use bit manipulation

--- 338.cc
+++ 338.cc
@@ -10,7 +10,7 @@ public:
         vector<int> bits(num + 1, 0);
         for (int i = 1; i <= num; ++i) {
             if (i & 1)  bits[i] = bits[i - 1] + 1;
-            else    bits[i] = bits[i / 2];
+            else    bits[i] = bits[i >> 1];
         }
         return bits;
     }
